fc6e8af4456e89985b7865024b940d7ce73edc7c
NguyenThanhTrung <105348335+nttrung2406@users.noreply.github.com>
2025-03-03 16:53:47 +0700

    search query with clip

--- backend/routes/searchRoutes.py
+++ backend/routes/searchRoutes.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Query
-from services.vectorStore import search_documents
+from fastapi import APIRouter
+from services.vectorStore import search_embedding
+from models.model import clip_model
 
 router = APIRouter()
 
-@router.get("/")
-async def search(query: str = Query(...)):
-    results = search_documents(query)
+@router.get("/search")
+def search(query: str):
+    query_embedding = clip_model.get_text_embedding(query)
+    results = search_embedding(query_embedding)
     return {"results": results}
